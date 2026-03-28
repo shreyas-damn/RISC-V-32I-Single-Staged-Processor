af4dc7e1066da4f23200b9f790e37c7228181be6
Shreyas J <supershreyas2006@gmail.com>
2026-03-29 00:03:41 +0530

    d_flip_flop

--- reg_32/dff.v
+++ reg_32/dff.v
@@ -1,16 +1,16 @@
 `timescale 1ns/1ps
 module dff(
-    input clk, din,
-    output q, q_bar
+    input din, clk,
+    output reg q, 
+    output qbar
 );
-wire nand1_op;
-wire nand2_op;
-wire w1;
-
-nand m1 (nand1_op, din, clk);
-not m2 (w1, din);
-nand m3 (nand2_op, w1, clk);
-nand m4 (q, nand1_op, q_bar);
-nand m5 (q_bar, nand2_op, q);  
-
+always @(posedge clk) begin
+    if (din == 1) begin
+        q <= 1; 
+    end
+    else begin
+        q <= 0;
+    end
+end
+    assign qbar = ~q;
 endmodule
--- reg_32/dff_tb.v
+++ reg_32/dff_tb.v
@@ -1,24 +1,27 @@
 `timescale 1ns/1ps
 module dff_tb();
-reg din_tb;
-reg clk_tb;
-wire q_tb;
-wire q_bar_tb;
+reg din_tb, clk_tb;
+wire q_tb, qbar_tb;
 
 dff uut(
     .din(din_tb),
     .clk(clk_tb),
     .q(q_tb),
-    .q_bar(q_bar_tb)
+    .qbar(qbar_tb)
 );
+
 initial begin
     $dumpfile("sim.vcd");
-    $dumpvars(0,dff_tb);
-    $monitor("Time = %t | clk = %b | din = %b | q = %b | q_bar = %b", $time, clk_tb, din_tb, q_tb, q_bar_tb);
+    $dumpvars(0, dff_tb);
+    $monitor("Time = %t | clk = %b | din = %b | q = %b | qbar = %b", $time, clk_tb, din_tb, q_tb, qbar_tb);
     clk_tb = 0; din_tb = 0; #10;
     clk_tb = 1; din_tb = 1; #10;
     clk_tb = 0; din_tb = 0; #10;
     clk_tb = 1; din_tb = 1; #10;
-    $finish;    
+    clk_tb = 0; din_tb = 1; #10;
+    clk_tb = 1; din_tb = 0; #10;
+    clk_tb = 0; din_tb = 1; #10;
+    clk_tb = 1; din_tb = 1; #10;
+    $finish;
 end
 endmodule
